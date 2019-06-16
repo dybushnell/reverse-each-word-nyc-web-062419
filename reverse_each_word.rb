664a5c402f11df1a824a3763f477fe67cbf2a593
github username <github email address>
2019-06-15 23:22:01 -0400

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+#def reverse_each_word(sentence)
+#    array = []
+#    array = sentence.split(' ')
+#    array.each do |word|
+#        word.reverse!
+#    end
+#    return array.join(' ')
+#end
+    
+
+
+def reverse_each_word(sentence)
+    array = []
+    array = sentence.split(' ')
+    array.collect do |word|
+        word.reverse!
+    end
+    return array.join(' ')
+end
